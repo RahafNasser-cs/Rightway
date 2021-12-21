dd94438e504e9d57faf0776dfbbfa1770e3ace97
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-12-21 13:44:32 +0300

    create AuthFragment UI

---
+++ app/src/main/java/com/rahafcs/co/rightway/AuthFragment.kt
@@ -0,0 +1,8 @@
+package com.rahafcs.co.rightway
+
+import androidx.fragment.app.Fragment
+import com.rahafcs.co.rightway.databinding.FragmentAuthBinding
+
+class AuthFragment : Fragment() {
+    lateinit var binding: FragmentAuthBinding
+}
