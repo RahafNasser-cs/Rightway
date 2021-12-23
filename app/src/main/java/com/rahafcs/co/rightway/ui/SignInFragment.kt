9aab6404c757ae45dbceaf5554cb9af7ed0be434
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-12-23 13:05:21 +0300

    add welcome page

---
+++ app/src/main/java/com/rahafcs/co/rightway/ui/SignInFragment.kt
@@ -0,0 +1,28 @@
+package com.rahafcs.co.rightway.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.rahafcs.co.rightway.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+    var binding: FragmentSignInBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.title = "Sign in"
+    }
+}
